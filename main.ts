d7b40bd667273c28b22cadcd2a4ce748cfa32175
HaraT0323 <alra9si916323@gmail.com>
2022-10-28 22:55:52 +0900

    KOTログイン処理追加

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
-import { webkit } from 'playwright'
+import { webkit } from "playwright";
 
 (async () => {
-  const browser = await webkit.launch()
-  const page = await browser.newPage()
-  await page.goto('https://s2.kingtime.jp/admin')
-  await page.screenshot({ path: `example.png` })
-  await browser.close()
-})()
+  const browser = await webkit.launch();
+  const page = await browser.newPage();
+
+  // KOTのログイン処理
+  await page.goto("https://s2.kingtime.jp/admin");
+  await page.fill("#login_id", process.env.KOT_LOGIN_ID || "");
+  await page.fill("#login_password", process.env.KOT_LOGIN_PASSWORD || "");
+  await page.click("#login_button");
+  await page.waitForTimeout(5000);
+
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
+})();
