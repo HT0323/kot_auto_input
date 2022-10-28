bab85f849c2be3bc41a8864252d779f5428885cf
HaraT0323 <alra9si916323@gmail.com>
2022-10-28 23:53:41 +0900

    打刻編集画面への遷移処理追加

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { webkit } from "playwright";
+import { chromium } from "playwright";
 
 (async () => {
-  const browser = await webkit.launch();
+  const browser = await chromium.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
 
   // KOTのログイン処理
@@ -9,8 +9,14 @@ import { webkit } from "playwright";
   await page.fill("#login_id", process.env.KOT_LOGIN_ID || "");
   await page.fill("#login_password", process.env.KOT_LOGIN_PASSWORD || "");
   await page.click("#login_button");
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(2000);
 
+  // 打刻編集画面への遷移
+  await page.click(".htBlock-selectOther >> nth=2");
+  await page.locator("select >> nth=2").selectOption({ label: "打刻編集" });
+
+  await page.waitForTimeout(4000);
   await page.screenshot({ path: `example.png` });
+
   await browser.close();
 })();
