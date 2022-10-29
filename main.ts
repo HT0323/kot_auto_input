d8daf2a16b4c4a4ce0c97e39008d318869fb1aaa
HaraT0323 <alra9si916323@gmail.com>
2022-10-29 10:38:29 +0900

    各種時刻の入力

--- main.ts
+++ main.ts
@@ -9,13 +9,32 @@ import { chromium } from "playwright";
   await page.fill("#login_id", process.env.KOT_LOGIN_ID || "");
   await page.fill("#login_password", process.env.KOT_LOGIN_PASSWORD || "");
   await page.click("#login_button");
-  await page.waitForTimeout(2000);
 
   // 打刻編集画面への遷移
-  await page.click(".htBlock-selectOther >> nth=2");
-  await page.locator("select >> nth=2").selectOption({ label: "打刻編集" });
+  await page.click(".htBlock-selectOther >> nth=3");
+  await page.locator("select >> nth=4").selectOption({ label: "打刻編集" });
 
-  await page.waitForTimeout(4000);
+  // 出勤時刻の入力
+  await page.locator("#recording_type_code_1").selectOption({ label: "出勤" });
+  await page.fill("#recording_timestamp_time_1", "1000");
+
+  // 退勤時刻の入力
+  await page.locator("#recording_type_code_2").selectOption({ label: "退勤" });
+  await page.fill("#recording_timestamp_time_2", "1900");
+
+  // 休憩開始時刻の入力
+  await page
+    .locator("#recording_type_code_3")
+    .selectOption({ label: "休憩開始" });
+  await page.fill("#recording_timestamp_time_3", "1200");
+
+  // 休憩終了時刻の入力
+  await page
+    .locator("#recording_type_code_4")
+    .selectOption({ label: "休憩終了" });
+  await page.fill("#recording_timestamp_time_4", "1300");
+
+  await page.waitForTimeout(1000);
   await page.screenshot({ path: `example.png` });
 
   await browser.close();
