9450b31657e1d1b2ce5c03108ddbf326f76dc6d0
HaraT0323 <alra9si916323@gmail.com>
2022-11-15 22:07:56 +0900

    環境変数チェック関数の実装 #10

---
+++ check_env.ts
@@ -0,0 +1,17 @@
+/** 環境変数を未定義かチェックする*/
+export const checkEnvVar = (): {
+  kotLoginId: string;
+  kotLoginPassword: string;
+} => {
+  const kotLoginId = process.env.KOT_LOGIN_ID || "";
+  const kotLoginPassword = process.env.KOT_LOGIN_PASSWORD || "";
+
+  if (!(kotLoginId !== "" && kotLoginPassword !== "")) {
+    console.log(kotLoginId, kotLoginPassword);
+    throw new Error(
+      "undefined KOT_LOGIN_ID & KOT_LOGIN_PASSWORD environment variable"
+    );
+  }
+
+  return { kotLoginId: kotLoginId, kotLoginPassword: kotLoginPassword };
+};
--- main.ts
+++ main.ts
@@ -1,100 +1,107 @@
 import { AutoBrowserOperation } from "./auto_browser_operation";
+import { checkEnvVar } from "./check_env";
 require("dotenv").config();
 
 async function main() {
-  // 毎日の勤怠情報を保持する、今は固定値だがCSVファイルから読み込んだ値で作成できるようにする。
-  let attendance: {
-    /** 日付 */
-    [key: string]: {
-      /** 勤務開始時刻 */
-      startTime: string;
+  try {
+    const envVar = checkEnvVar();
 
-      /** 勤務終了時刻 */
-      endTime: string;
+    // 毎日の勤怠情報を保持する、今は固定値だがCSVファイルから読み込んだ値で作成できるようにする。
+    let attendance: {
+      /** 日付 */
+      [key: string]: {
+        /** 勤務開始時刻 */
+        startTime: string;
+
+        /** 勤務終了時刻 */
+        endTime: string;
+      };
+    } = {
+      "3": {
+        startTime: "1000",
+        endTime: "1930",
+      },
+      // "4": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "5": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "6": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "7": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "11": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "12": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "13": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "14": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "17": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "18": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "19": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "20": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "21": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "24": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "25": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "26": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "27": {
+      //   startTime: "1000",
+      //   endTime: "1930",
+      // },
+      // "28": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
+      // "31": {
+      //   startTime: "1000",
+      //   endTime: "1900",
+      // },
     };
-  } = {
-    "3": {
-      startTime: "1000",
-      endTime: "1930",
-    },
-    // "4": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "5": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "6": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "7": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "11": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "12": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "13": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "14": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "17": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "18": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "19": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "20": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "21": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "24": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "25": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "26": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "27": {
-    //   startTime: "1000",
-    //   endTime: "1930",
-    // },
-    // "28": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-    // "31": {
-    //   startTime: "1000",
-    //   endTime: "1900",
-    // },
-  };
 
-  await AutoBrowserOperation(attendance);
+    await AutoBrowserOperation(attendance);
+  } catch (error) {
+    console.log(`エラー発生 ${error}`);
+  }
 }
 main();
